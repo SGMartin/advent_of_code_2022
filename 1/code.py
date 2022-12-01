7495d3ddb27b40e9ba0963ae1bd65e34fdf82d22
Santiago García Martín <s.g.martin94@gmail.com>
2022-12-01 11:00:32 +0100

    Add day 1

---
+++ 1/code.py
@@ -0,0 +1,12 @@
+
+## Generate a list of list where each list is an elf cal.count
+with open("puzzle_input.txt", "r") as cals:
+  all_lines = cals.read().rstrip() ## Get rif of final new line if I managed to copy it
+  elfs = [line.split("\n") for line in all_lines.split("\n\n")]
+
+## Cast to int each elf cals. and sum them to get a list of total cals/elf
+all_elfs = [sum(map(int, elf_cals)) for elf_cals in elfs]
+
+
+print(f"Day 1/1 answer is: {sorted(all_elfs, reverse=True)[0]} calories")
+print(f"Day 1/2 answer is: {sum(sorted(all_elfs, reverse=True)[0:3])} calories")
---
+++ README.md
@@ -0,0 +1,3 @@
+# Advcent of Code 2022
+
+* Day 1 (December 1, 2022): [Calorie Counting](https://adventofcode.com/2022/day/1) - [Solution]()
