a810d5697008222ef59cb761fb6bd097d07f84fa
Santiago García Martín <s.g.martin94@gmail.com>
2022-12-05 11:08:56 +0100

    Add day 5

---
+++ 5/code.py
@@ -0,0 +1,92 @@
+import copy
+import re
+
+## NOTE: most of the solutions I've seen place the stacks manually in the code
+## I refuse to that kind of stuff so let the parsing begin
+
+## scan n. of crates. The previous lines will be the crate
+## arrangement
+stacked_crates = 0
+crane_orders = []
+crate_numbering = []
+crate_lines = []
+
+
+def crane_crates(crate_stack: list, crane_cmd: list, from_top: bool = True) -> list:
+    # position 0 -> crates to move
+    # position 1 -> origin
+    # position 2 -> new crate columns
+    new_stack = crate_stack
+    n_crates = int(crane_cmd[0])
+    old_col = int(crane_cmd[1])
+    new_col = int(crane_cmd[2])
+
+    crates_to_move = new_stack[old_col][-n_crates:]
+    if from_top:
+        crates_to_move.reverse()
+    
+    new_stack[new_col].extend(crates_to_move)
+    new_stack[old_col] = new_stack[old_col][:len(new_stack[old_col]) - n_crates]
+
+    return new_stack
+
+
+## We'll split the input in three parts: 
+# a) Find the crate column numbering
+# b) Find the crane orders
+# c) The top of the file, the crate columns themselves
+
+with open("puzzle_input.txt", "r") as fil:
+   
+   for idx, line in enumerate(fil.readlines()):
+        if line.startswith(" 1"):
+            col_array = [x for x in line.rstrip().split(" ")]
+            col_array = [int(x) for x in col_array if x != ""]
+            stacked_crates = max(col_array)
+            crate_numbering = [line.rstrip().index(str(x)) for x in col_array]
+            continue
+        if line.startswith("move"):
+            crane_orders.append(line.rstrip())
+            continue
+        if line.rstrip() != "":
+            crate_lines.append(line.rstrip())
+
+
+crates = {k:[] for k in range(1, stacked_crates + 1)}
+crate_char_pos = dict(zip(range(1, stacked_crates + 1), crate_numbering))
+
+for crate_line in crate_lines:
+    for crate, pos in crate_char_pos.items():
+        if len(crate_line) >= pos:
+            if crate_line[pos] != " ":
+                crates[crate].append(crate_line[pos])
+
+
+for key,val in crates.items():
+    crates[key].reverse()
+
+## We'll need a copy for part two
+crates_reordered = copy.deepcopy(crates)
+
+for crane_order in crane_orders:
+    orders = re.findall(r'\d+', crane_order)
+    crates_reordered = crane_crates(crates_reordered, orders, from_top=True)
+    
+last_crates = []
+for crate_id, crate_col in crates_reordered.items():
+    last_crates.append(crates_reordered[crate_id].pop())
+
+last_crates = "".join(last_crates)
+print(f"Part 1 answer is: {last_crates}")
+
+## Part 2
+for crane_order in crane_orders:
+    orders = re.findall(r'\d+', crane_order)
+    crates = crane_crates(crates, orders, from_top=False) # this is the only dif. 
+
+last_crates_part2 = []
+for crate_id, crate_col in crates.items():
+    last_crates_part2.append(crates[crate_id].pop())
+
+last_crates_part2 = "".join(last_crates_part2)
+print(f"Part 2 answer is: {last_crates_part2}")
--- README.md
+++ README.md
@@ -22,6 +22,7 @@
 * Day 2 (December 2, 2022): [Rock Paper Scissors](https://adventofcode.com/2022/day/2) - [Solution](https://github.com/SGMartin/advent_of_code_2022/blob/main/2/code.py)
 * Day 3 (December 3, 2022): [Rucksack Reorganization](https://adventofcode.com/2022/day/3) - [Solution](https://github.com/SGMartin/advent_of_code_2022/blob/main/3/code.py)
 * Day 4 (December 4, 2022): [Camp Cleanup](https://adventofcode.com/2022/day/4) - [Solution](https://github.com/SGMartin/advent_of_code_2022/blob/main/4/code.py)
+* Day 5 (December 5, 2022): [Supply Stacks](https://adventofcode.com/2022/day/5) - [Solution](https://github.com/SGMartin/advent_of_code_2022/blob/main/5/code.py)
 
 
 _Answers by SGMartin._
