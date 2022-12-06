928edcd617383a6570f71dcfe81d42a48b2c6681
Santiago Garcia <s.g.martin94@gmail.com>
2022-12-06 16:40:37 +0100

    Remove unique and use sets

--- 6/code.py
+++ 6/code.py
@@ -1,17 +1,8 @@
 with open("puzzle_input.txt", "r") as lines:
     buffer = lines.readline().rstrip()  ## a single string
 
-
 initial_pos = 0
 end_pos = 4
-
-
-def check_unique(str: str) -> bool:
-    for i in range(len(str)):
-        for j in range(i + 1, len(str)):
-            if str[i] == str[j]:
-                return False
-    return True
 
 
 def find_first_position(datastream: str, packet_size: int = 4) -> int:
@@ -22,7 +13,7 @@ def find_first_position(datastream: str, packet_size: int = 4) -> int:
     while end_pos < len(datastream):
         candidate_seq = datastream[initial_pos:end_pos]
 
-        if check_unique(candidate_seq):
+        if len(set(candidate_seq)) == packet_size:
             break
         else:
             end_pos += 1
