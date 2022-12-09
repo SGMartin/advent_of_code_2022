60472a620689712e469f0702a22b75ec993ab860
Santiago Garcia <s.g.martin94@gmail.com>
2022-12-09 17:30:34 +0100

    Format in black for day 9

--- 9/code.py
+++ 9/code.py
@@ -2,15 +2,15 @@ with open("puzzle_input.txt", "r") as fil:
     moves = [mov.rstrip() for mov in fil.readlines()]
 
 move_knot = {
-    "R": lambda row, col : (row + 1, col),
-    "L": lambda row, col : (row - 1, col),
-    "U": lambda row, col : (row, col + 1),
-    "D": lambda row, col : (row, col - 1),
+    "R": lambda row, col: (row + 1, col),
+    "L": lambda row, col: (row - 1, col),
+    "U": lambda row, col: (row, col + 1),
+    "D": lambda row, col: (row, col - 1),
 }
 
 ## the rope has 10 knots. We are going to use the code from part 2 to solve
 ## part 1. It's just a generalization
-knots = 10 
+knots = 10
 rope = [(0, 0) for knot in range(0, knots)]
 knot_movements = [set() for knot in range(0, knots)]
 
@@ -22,18 +22,18 @@ for move in moves:
 
     for _ in range(steps):
         for kid, kpos in enumerate(rope):
-            if kid == 0: ## the head!!!!
+            if kid == 0:  ## the head!!!!
                 # Unpack row, col, less typing. Identical to rope[kid][0] and
                 # rope[kid][1]
-                rope[kid] = move_knot[direction](*rope[kid]) 
-                knot_movements[kid].add(rope[kid]) ## not further ado for the head
-            else: ## not the head, but one of the subsequent knots
+                rope[kid] = move_knot[direction](*rope[kid])
+                knot_movements[kid].add(rope[kid])  ## not further ado for the head
+            else:  ## not the head, but one of the subsequent knots
                 prev_knot = rope[kid - 1]
                 delta_row = prev_knot[0] - kpos[0]
                 delta_col = prev_knot[1] - kpos[1]
-                
+
                 ## Illegal position
-                if abs(delta_row) > 1 or abs(delta_col) > 1: 
+                if abs(delta_row) > 1 or abs(delta_col) > 1:
 
                     ## Taking this shorcut from a reddit user. It removes
                     ## a lot of boiler plate code about diagonals. Basically,
